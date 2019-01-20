32b6d7072e94406fd1da989831d130f997ef88e2
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-01-20 17:04:38 -0500

    add error handling for article not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+protected
+# leaving this empty to define in individual controllers
+  def resource_not_found
+  end
 end
