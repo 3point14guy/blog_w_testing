606e7f49226fd42b09acea8e229ce2c8f5491321
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-01-20 15:12:02 -0500

    implement feature to show message when no articles exist

--- spec/feature/list_article_spec.rb
+++ spec/feature/list_article_spec.rb
@@ -8,13 +8,26 @@ RSpec.feature "List Articles" do
 
   scenario "List all articles when visiting root" do
     visit '/'
-
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
     expect(page).to have_content(@article2.title)
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+  end
 
+  scenario "A user has no articles when visiting root" do
+    Article.delete_all
+    visit '/'
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within("h3#no-articles") do
+      expect(page).to have_content("No Articles have been created.")
+    end
   end
 end
