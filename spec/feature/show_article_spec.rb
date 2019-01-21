31261efb27391e05b60f79863e551eac05a80e4a
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-01-21 08:37:50 -0500

    fix broken show article spec

--- spec/feature/show_article_spec.rb
+++ spec/feature/show_article_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Show Article" do
   before do
-    @article = Article.create(title: "BDD article", body: "Behavior driven developemnt is more robust than unit testing bc we are testing how we expect users to interact with the app.")
+    user = User.create!(email: "name@mail.com", password: "secret", password_confirmation: "secret")
+    @article = Article.create(title: "BDD article", body: "Behavior driven developemnt is more robust than unit testing bc we are testing how we expect users to interact with the app.", user: user)
   end
 
   scenario "clicking article title takes user to article show page" do
