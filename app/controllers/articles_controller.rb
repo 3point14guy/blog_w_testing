b041263768d14b3bc523eb8b16cb5353aaecba63
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-01-20 19:56:54 -0500

    refactor controller, new & edit views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, except: [:index, :new, :create]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = "Article deleted successfully."
       redirect_to articles_path
@@ -18,11 +17,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article has been updated."
       redirect_to @article
@@ -56,6 +53,9 @@ protected
   end
 
 private
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
