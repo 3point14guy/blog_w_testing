c64f6df9776843b71de436bd84c7847da6f475e6
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-01-22 23:22:24 -0500

    restrict edit and delete article links to article author only.

--- spec/feature/show_article_spec.rb
+++ spec/feature/show_article_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
-RSpec.feature "Show Article" do
+RSpec.feature "Showing Article" do
   before do
-    user = User.create!(email: "name@mail.com", password: "secret", password_confirmation: "secret")
-    @article = Article.create(title: "BDD article", body: "Behavior driven developemnt is more robust than unit testing bc we are testing how we expect users to interact with the app.", user: user)
+    @user1 = User.create!(email: "name1@mail.com", password: "secret", password_confirmation: "secret")
+    @user2 = User.create!(email: "name2@mail.com", password: "secret", password_confirmation: "secret")
+    @article = Article.create(title: "BDD article", body: "Behavior driven developemnt is more robust than unit testing bc we are testing how we expect users to interact with the app.", user: @user1)
   end
 
-  scenario "clicking article title takes user to article show page" do
+  scenario "to non-signed in user hides edit and delete" do
     visit '/'
     click_link(@article.title)
     expect(page.current_path).to eq(article_path(@article))
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "to non-author, signed in user hides edit and delete" do
+    login_as(@user2)
+    visit '/'
+    click_link(@article.title)
+    expect(page.current_path).to eq(article_path(@article))
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "to signed in author shows edit and delete" do
+    login_as(@user1)
+    visit '/'
+    click_link(@article.title)
+    expect(page.current_path).to eq(article_path(@article))
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(page).to have_content "Edit Article"
+    expect(page).to have_content "Delete Article"
   end
 end
